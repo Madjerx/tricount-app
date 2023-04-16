38f581b0b0998282ed3ec9b07088ee0140305c42
Madjerx <madjerx@hotmail.fr>
2023-04-16 11:39:57 +0200

    MVP okay. now I have to improve css and UX/UI

--- src/app/components/depenses-table/depenses-table.component.ts
+++ src/app/components/depenses-table/depenses-table.component.ts
@@ -8,24 +8,49 @@ import { Depense } from 'src/app/models/depense';
 })
 export class DepensesTableComponent {
   public depenses: Depense[] = [];
-  public mappedDepenses: Map<string, number> = new Map<string, number>(); 
+  public mappedDepenses: Map<string, number> = new Map<string, number>();
+  public indebted: string = '';
+  public creditor: string = '';
+  public amountToGive: number = 0;
+  public fileLoaded: boolean = false;
 
-  public constructor(){};
 
-  public addDepenses(depenses: Depense[]):void {
+  public constructor() { };
+
+  public addDepenses(depenses: Depense[]): void {
     this.depenses = depenses;
-   
+    this.fileLoaded = true;
+
     // Group expenses by author in a Map variable
 
     this.depenses.forEach(expenses => {
-      if(this.mappedDepenses.has(expenses.getName())) {
-        this.mappedDepenses.set(expenses.getName(), expenses.getRate()*expenses.getSpent()+this.mappedDepenses.get(expenses.getName())!)
+      if (this.mappedDepenses.has(expenses.getName())) {
+        this.mappedDepenses.set(expenses.getName(), expenses.getRate() * expenses.getSpent() + this.mappedDepenses.get(expenses.getName())!)
       } else {
-        this.mappedDepenses.set(expenses.getName(), expenses.getRate()*expenses.getSpent());
+        this.mappedDepenses.set(expenses.getName(), expenses.getRate() * expenses.getSpent());
       }
     })
 
-    console.log('map',this.mappedDepenses);
+
+    // Get author in debt
+
+    const authorArray: string[] = [...this.mappedDepenses.keys()];
+    console.log(authorArray);
+    if (this.mappedDepenses.get(authorArray[0])! > this.mappedDepenses.get(authorArray[1])!) {
+      this.indebted = authorArray[1];
+      this.creditor = authorArray[0];
+    } else {
+      this.indebted = authorArray[0];
+      this.creditor = authorArray[1];
+    }
+
+    // Get amount to give
+
+    this.amountToGive = Math.abs(this.mappedDepenses.get(authorArray[0])! - this.mappedDepenses.get(authorArray[1])!)
+
+
+
+
 
   }
 }
